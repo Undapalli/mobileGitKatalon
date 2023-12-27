c67a3d4191ebb70e0cdecab482eb1ce7973cdb58
Undapalli <undapallijyothi@gmail.com>
2023-12-27 22:42:41 +0530

    test suite collection is pushed

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>a9ce3b54-7a68-4363-8299-95ab30ea1fd0</testSuiteGuid>
+   <testCaseLink>
+      <guid>dfd7e176-3fda-4ba7-b428-df51eb1bb4ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
